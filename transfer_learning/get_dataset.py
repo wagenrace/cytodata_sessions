1d579927d6e20ba3d17b24bbb82183a1330f1bda
tomnijhof <tom.nijhof@cytosmart.com>
2021-01-27 07:45:42 +0100

    update get_dataset to deal with png
    Signed-off-by: tomnijhof <tom.nijhof@cytosmart.com>

--- transfer_learning/get_dataset.py
+++ transfer_learning/get_dataset.py
@@ -24,6 +24,7 @@ class DataSetGenerator:
     data_location:str=r"D:\data\food"
     batch_size:int=64
     patch_dimension:int=128
+    image_type:str="jpg"
 
     def get_label(self, file_path):
         # Transform label name in one-hot encoded label
@@ -33,7 +34,10 @@ class DataSetGenerator:
 
     def decode_img(self, img, extra_padding=0):
         # Decode and resize image
-        img = tf.image.decode_jpeg(img, channels=3)
+        if self.image_type == "jpg":
+            img = tf.image.decode_jpeg(img, channels=3)
+        else:
+            img = tf.image.decode_png(img, channels=3)
         img_size = self.patch_dimension + extra_padding
         img = tf.image.resize(img, (img_size, img_size))
         return img
@@ -81,8 +85,13 @@ class DataSetGenerator:
 
         # Load all data locations
         data_dir = pathlib.Path(self.data_location)
-        image_count = len(list(data_dir.glob("*/*.jpg")))
-        list_ds = tf.data.Dataset.list_files(str(data_dir / "*/*"), shuffle=False)
+        if self.image_type == "jpg":
+            image_count = len(list(data_dir.glob("*/*.jpg")))
+            list_ds = tf.data.Dataset.list_files(str(data_dir / "*/*.jpg"), shuffle=False)
+
+        else:
+            image_count = len(list(data_dir.glob("*/*.png")))
+            list_ds = tf.data.Dataset.list_files(str(data_dir / "*/*.png"), shuffle=False)
 
         # Get all unique class names
         self.class_names = np.array(
